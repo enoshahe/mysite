056fe26ce3296f97555a05ba652f4ecfed227b73
enosha <eno@ucsc.cmb.ac.lk>
2016-08-16 09:55:27 +0000

    initial add

---
+++ polls/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+def was_published_recently(self):
+       return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+was_published_recently.admin_order_field = 'pub_date'
+was_published_recently.boolean = True
+was_published_recently.short_description = 'Published recently?'
+
+list_filter = ['pub_date']
+
+search_fields = ['question_text']
+
